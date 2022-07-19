8e5ac5689f8e471b68d73f46d657b7aa41d6ac72
David Moore <davemooreuws@gmail.com>
2022-07-19 14:56:49 +1000

    chore: test 8

--- functions/hello.ts
+++ functions/hello.ts
@@ -3,7 +3,7 @@ import { api } from "@nitric/sdk";
 const helloApi = api("main");
 
 helloApi.get("/hello", async (ctx) => {
-  ctx.res.json({ env: process.env });
+  ctx.res.json({ env: process.env, cool: 1 });
 
   return ctx;
 });
